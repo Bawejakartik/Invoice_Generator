27a968ffed65014f8b42d5da296dc0ae8642f473
kartik <kartikbaweja16@gmail.com>
2026-06-24 16:47:03 +0530

    index.js changed completely

--- server/Routes/DashboardRoutes.js
+++ server/Routes/DashboardRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const route = express.Router();
-const { getDashboardSummary } = require("../controller/dashboardController");
+const { getDashboardSummary } = require("../controller/DashboardController");
 const isAuthenticated = require("../middleware/authMiddleware");
 
 route.get("/dashboard-summary", isAuthenticated, getDashboardSummary);
