8125e34526033d39315a16b3f05211d5a1536383
kartik <kartikbaweja16@gmail.com>
2026-06-26 09:33:16 +0530

    production email transporter is changed

--- server/config/transporter.js
+++ server/config/transporter.js
@@ -1,11 +1,22 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config()
-const transporter = nodemailer.createTransport({
-service: "gmail",
-auth: {
-user: process.env.GMAIL_USER,
-pass: process.env.GMAIL_APP_PASSWORD,
-},
-});
+// const transporter = nodemailer.createTransport({
+// service: "gmail",
 
+// auth: {
+// user: process.env.GMAIL_USER,
+// pass: process.env.GMAIL_APP_PASSWORD,
+// },
+// });
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_APP_PASSWORD,
+  },
+});
 module.exports = transporter;
